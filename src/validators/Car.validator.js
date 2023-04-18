f84d7ce8d9d7e52a9a3483193755d132e99f3fcf
YehorSaz <inercoin@gmail.com>
2023-04-18 14:19:28 +0300

    homework lesson3

--- src/validators/Car.validator.js
+++ src/validators/Car.validator.js
@@ -3,7 +3,6 @@ import Joi from "joi";
 const carValidator = Joi.object({
     brand:Joi.string().regex(/^[a-zA-Zа-яА-яёЁіІїЇ]{1,20}$/).required().messages({
         'string.pattern.base':'wrong data',
-        'string.required':'field is required'
     }),
     price:Joi.number().min(0).max(1000000).required(),
     year:Joi.number().min(1990).max(new Date().getFullYear()).required()
--- src/validators/Comments.validator.js
+++ src/validators/Comments.validator.js
@@ -2,7 +2,9 @@ import Joi from "joi";
 
 const commentsValidator = Joi.object({
     name:Joi.string().required(),
-    email:Joi.string().regex(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/).required(),
+    email:Joi.string().regex(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/).required().messages({
+        'string.pattern.base':'wrong data'
+    }),
     body:Joi.string().required()
 });
 
